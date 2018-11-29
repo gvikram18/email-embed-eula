47bbd97efeed98cffdac258071a3a3fe4b676a1a
Wiki_theDev <gvikram18@gmail.com>
2018-11-29 17:35:23 +0530

    done with styling

---
+++ verified.php
@@ -0,0 +1,45 @@
+<?php  
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email verified</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.5/css/fork-awesome.min.css" integrity="sha256-P64qV9gULPHiZTdrS1nM59toStkgjM0dsf5mK/UwBV4=" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div class="jumbotron text-xs-center" style="background: none; background-color: #f9f9f9;
+    margin-bottom: 0;`enter code here`
+    min-height: 50%;
+    background-repeat: no-repeat;
+    background-position: center;
+    -webkit-background-size: cover;
+    background-size: cover;
+">
+  <h1 class="display-3" style="text-align: center;color: #8ecd4e">Your email has been verified!</h1>
+  <p class="lead" style="text-align: center; color: #054978;"><strong>Please wait</strong> for further instructions on how to complete your account setup.</p>
+  <hr>
+  <p style="text-align: center;">
+    Having trouble? Mail us : <a href="#" style="color: #054978;"> support@ivtree.com</a>
+  </p>
+  <p class="lead" style="text-align: center;">
+    <button class="btn btn-primary btn-sm" style="background-color: #054978; border-radius: 17px; width: 200px;height: 50px;font-size: 16px;" href="https://uniclair.com/" role="button">Continue to homepage</button>
+  </p>
+</div>
+<div style="margin:30px auto;text-align:center;">
+	<br><br>
+<hr>
+
+<a href="#" target="_blank"><img src="https://ivtree.com/wp-content/uploads/2017/04/fb-cover-7913-58d95c2f59692.jpg" alt="IVTREE" width="200"></a><br><br>
+<p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">© IVTREE IT Solutions.</p><p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">ThinkHub, 100-103 Export Promotion Industrial Park, Whitefield, Bengaluru, Karnataka 560066</p>
+
+<a href="https://in.linkedin.com/company/ivtree" target="_blank"><i class="fa fa-linkedin-square fa-stack-2x" aria-hidden="true"></i></a>
+
+</div>
+</body>
+</html>
