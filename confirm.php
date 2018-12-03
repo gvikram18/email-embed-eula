93c709bd40f219b5fb403a8e5e1d2e5610e358c6
Wiki_theDev <gvikram18@gmail.com>
2018-12-03 16:24:46 +0530

    Add files via upload

--- confirm.php
+++ confirm.php
@@ -1,30 +1,39 @@
 <?php
 
-	function redirect() {
-		header('Location: register.php');
+	function userpresent() {
+		header('Location: userpresent.php');
 		exit();
 	}
 	function verified() {
 		header('Location: verified.php');
-		
+		exit();                                  //only two listed functions work. More than two, will face glitch.
+	}
+	function error() {
+		header('Location: error.php');
+		exit();
 	}
 
-	if (!isset($_GET['email']) || !isset($_GET['token'])) {
-		redirect(); #contact support@ivtree.com
-	} else {
+	
+
+	if (!isset($_GET['email']) || !isset($_GET['token']))
+		error(); #contact support@ivtree.com 'Error establishing database connection'
+	
+	else {
 		$con = new mysqli('localhost', 'root', 'ivtree123', 'action-plan');
 
 		$email = $con->real_escape_string($_GET['email']);
 		$token = $con->real_escape_string($_GET['token']);
+		
 
-		$sql = $con->query("SELECT id FROM users WHERE email='$email' AND token='$token' AND isEmailConfirmed=0 AND responded='$responded'");
+		$sql = $con->query("SELECT id FROM users WHERE email='$email' AND token='$token' AND isEmailConfirmed=0 AND responded='0000-00-00 00:00:00'");
 
 		if ($sql->num_rows > 0) {
 			date_default_timezone_set('Asia/Kolkata');
 			$responded=date("Y-m-d H:i:s");
 			$con->query("UPDATE users SET isEmailConfirmed=1, token='', responded='$responded' WHERE email='$email'");
 			verified();
-		} else
-			redirect();
+		} 
+		else
+			userpresent();		
 	}
 ?>
--- error.php
+++ error.php
@@ -1,5 +1,4 @@
-<?php  
-
+<?php
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,14 +11,15 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<script>
 		var myVar;
+
 		function myFunction() {
     		myVar = setTimeout(showPage, 2000);
 		}
 
 		function showPage() {
- 	   	    document.getElementById("loader").style.display = "none";
-            document.getElementById("myDiv").style.display = "block";
-    	}
+  			document.getElementById("loader").style.display = "none";
+  			document.getElementById("myDiv").style.display = "block";
+		}
 	</script>
 
 	<style>
@@ -52,19 +52,19 @@
   		100% { transform: rotate(360deg); }
 	}
 
-	/* Add animation to "page content" */
+/* Add animation to "page content" */
 	.animate-bottom {
   		position: relative;
   		-webkit-animation-name: animatebottom;
   		-webkit-animation-duration: 1s;
- 	    animation-name: animatebottom;
+  		animation-name: animatebottom;
   		animation-duration: 1s
 	}
 
 	@-webkit-keyframes animatebottom {
   		from { bottom:-100px; opacity:0 } 
   		to { bottom:0px; opacity:1 }
-	}
+		}
 
 	@keyframes animatebottom { 
   		from{ bottom:-100px; opacity:0 } 
@@ -81,23 +81,23 @@
 <body onload="myFunction()" style="margin:0;">
 	<div id="loader"></div>
 	<div style="display:none;" id="myDiv" class="animate-bottom">
-		<div class="jumbotron text-xs-center" style="background: none; background-color: #f9f9f9;margin-bottom: 0;min-height: 50%;background-repeat: no-repeat;background-position: center;-webkit-background-size: cover;background-size: cover;
-			padding: 2rem 2rem 4rem 4rem;">
-  			<h1 class="display-3" style="text-align: center;color: #8ecd4e;">Your email has been verified!</h1>
-  			<p class="lead" style="text-align: center; color: #054978;"><strong>Please wait</strong> for further instructions on how to complete your account setup.</p>
-  			<hr><br><br>
-  			<h3 class="display-5" style="text-align: center; color: #054978;"><strong>Close this page.</strong></h3><br>
-  			<p style="text-align: center;"> Having trouble? Mail us : <a href="mailto:support@ivtree.com" target="_top" style="color: #054978;"> support@ivtree.com</a>
-  			</p>
+		<div class="jumbotron text-xs-center" style="background: none; background-color: #f9f9f9;margin-bottom: 0;min-height: 50%;
+    background-repeat: no-repeat;background-position: center;-webkit-background-size: cover;background-size: cover;
+    padding: 0 2rem 4rem 4rem;">
+  		<h1 class="display-3" style="text-align: center;color: #fc0505;">Error establishing database connection for verification!</h1>
+  		<hr><br><br>
+  		<h4 class="lead" style="text-align: center; color: #054978;"><strong>Please Contact</strong> <a href="mailto:support@ivtree.com" target="_top" style="color: #054978;"> support@ivtree.com</a> for further action.</h4>
+  		<h3 class="display-5" style="text-align: center; color: #054978;"><strong>Close this page.</strong></h3><br>
 		</div>
-			<div style="margin-top:30px;text-align:center;">
-				<br><br>
-				<hr>
-				<a href="#" target="_blank"><img src="https://ivtree.com/wp-content/uploads/2017/04/fb-cover-7913-58d95c2f59692.jpg" alt="IVTREE" width="200"></a><br><br>
-				<p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">© IVTREE IT Solutions.</p><p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">ThinkHub, 100-103 Export Promotion Industrial Park, Whitefield, Bengaluru, Karnataka 560066</p>
+		<div style="margin-top:30px;text-align:center;">
+			<br><br>
+			<hr>
+			<a href="#" target="_blank"><img src="https://ivtree.com/wp-content/uploads/2017/04/fb-cover-7913-58d95c2f59692.jpg" alt="IVTREE" width="200"></a><br><br>
+			<p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">© IVTREE IT Solutions.</p><p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">ThinkHub, 100-103 Export Promotion Industrial Park, Whitefield, Bengaluru, Karnataka 560066</p>
 
-				<!--<a href="https://in.linkedin.com/company/ivtree" target="_blank"><i class="fa fa-linkedin-square fa-stack-2x" aria-hidden="true"></i></a>-->
-			</div>
+			<!--<a href="https://in.linkedin.com/company/ivtree" target="_blank"><i class="fa fa-linkedin-square fa-stack-2x" aria-hidden="true"></i></a>-->
+		</div>
 	</div>
+
 </body>
 </html>
